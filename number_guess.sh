b35053723da5479f4d2755c61966f53d0ab6534c
Kevin Huayllas <KevinOmega01@gmail.com>
2024-02-24 16:55:46 +0000

    feat: user doesnt exist case

--- number_guess.sh
+++ number_guess.sh
@@ -3,7 +3,20 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align --tuples-
 
 main(){
   RANDOM_NUMBER=$(( 1 + $RANDOM % 1000 ))
-  echo $RANDOM_NUMBER
+  echo "Enter your username:"
+  read USERNAME
+  #check if username exist
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE username = '$USERNAME'")
+  #if user does not exist
+  if [[ -z $USER_ID ]]
+  then
+    echo "Welcome, $USERNAME! It looks like this is your first time here."
+    USER_ID=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME') RETURNING user_id")
+    echo $USER_ID
+  else
+    echo "Already exist"
+  fi
+  #if user exist
 }
 
 main
