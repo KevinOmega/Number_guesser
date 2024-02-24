2ef937b8e92c6ad8f12eea6d45e41a6b02b494a0
Kevin Huayllas <KevinOmega01@gmail.com>
2024-02-24 17:11:24 +0000

    feat: user exist message

--- number_guess.sh
+++ number_guess.sh
@@ -12,9 +12,10 @@ main(){
   then
     echo "Welcome, $USERNAME! It looks like this is your first time here."
     USER_ID=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME') RETURNING user_id")
-    echo $USER_ID
   else
-    echo "Already exist"
+    NUMBER_OF_GAMES=$($PSQL "SELECT COUNT(*) FROM games WHERE user_id=$USER_ID")
+    MAX_SCORE=$($PSQL "SELECT MAX(guesses) FROM games WHERE user_id=$USER_ID")
+    echo "Welcome back, $USERNAME! You have played $NUMBER_OF_GAMES games, and your best game took $MAX_SCORE guesses."
   fi
   #if user exist
 }
