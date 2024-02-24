0082783e7ab588f3666995418266c4aa99843470
Kevin Huayllas <KevinOmega01@gmail.com>
2024-02-24 16:39:38 +0000

    feat: main function

--- number_guess.sh
+++ number_guess.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align --tuples-only -c"
 
-RANDOM_NUMBER=$(( 1 + $RANDOM % 1000 ))
-echo $RANDOM_NUMBER
+main(){
+  RANDOM_NUMBER=$(( 1 + $RANDOM % 1000 ))
+  echo $RANDOM_NUMBER
+}
+
+main
